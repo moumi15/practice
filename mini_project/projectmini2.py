58bc4123872e457e2bc8212ce015673cd3f3ffef
Moumi Saha <moumisaha2002@gmail.com>
2026-07-30 02:42:45 +0530

    randam password

--- mini_project/projectmini2.py
+++ mini_project/projectmini2.py
@@ -1,1 +1,27 @@
-#CREATE A RANDOM PASSWORD
+#CREATE A RANDOM PASSWORD USING LOOP
+
+import random
+import string
+
+pass_len = 12
+charvalues = string.ascii_letters + string.digits + string.punctuation
+
+password = ""
+for i in range(pass_len):
+    password += random.choice(charvalues)
+
+print("your password is : ", password)    
+
+
+#CREATE A RANDOM PASSWORD USING LIST COMPREHENSION
+
+import random
+import string
+
+pass_len = 12
+charvalues = string.ascii_letters + string.digits + string.punctuation
+
+password = "".join([random.choice(charvalues) for i in range(pass_len)])
+print("your password is : ", password) 
+
+ 
